352c85fe396a42efe8a5dd202771549130376457
LongAT hyperion <hypvn_partner10@hyperion.co.jp>
2021-09-16 15:56:05 +0700

    remove env

--- src/redux/auth/apiCall.ts
+++ src/redux/auth/apiCall.ts
@@ -1,16 +1,13 @@
-import env from '@env';
 import { API_CONFIG } from '@services/apiConfig';
 import withQuery from 'with-query';
 
 export async function requestLogin({ limit }): Promise<any> {
   try {
-    const url = withQuery(`${env.API_BASE}/${API_CONFIG.LOGIN}`, {
-      limit,
-    });
-
-    const response = await fetch(url);
-
-    return response.json();
+    // const url = withQuery(`${env.API_BASE}/${API_CONFIG.LOGIN}`, {
+    //   limit,
+    // });
+    // const response = await fetch(url);
+    // return response.json();
   } catch (error) {
     console.error('getAllFilms - Error: ', error);
     throw error;
