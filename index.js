4bc131cf8469defb14140bbd0be785528868d454
LongAT hyperion <hypvn_partner10@hyperion.co.jp>
2021-11-26 11:18:20 +0700

    add sentry

--- index.js
+++ index.js
@@ -6,10 +6,11 @@ import { AppRegistry, LogBox } from 'react-native';
 import { name as appName } from './app.json';
 import App from './src/App';
 import messaging from '@react-native-firebase/messaging';
+import * as Sentry from '@sentry/react-native';
 
 // Remove YellowBox on Debug application screen
 LogBox.ignoreAllLogs(true);
 messaging().setBackgroundMessageHandler(async remoteMessage => {
   console.log('Message handled in the background!', remoteMessage);
 });
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => Sentry.withTouchEventBoundary(App));
