ece29910a386e75535ba3d054175fce41650b219
LongNN hisoft <longnn17.developer@gmail.com>
2021-12-29 23:06:15 +0700

    fix bottom tab

--- src/components/CustomTabbar/index.tsx
+++ src/components/CustomTabbar/index.tsx
@@ -11,9 +11,10 @@ import commonStyles from '@theme/commonStyles';
 import { BlurView as Blur } from '@react-native-community/blur';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import { Images } from '@theme/images';
+import { useBottomTabStyle } from './styles';
 
 const CustomTabar: FC<BottomTabBarProps> = ({ state, descriptors, navigation }: BottomTabBarProps) => {
-  const styles = style();
+  const styles = useBottomTabStyle();
   const tabFocus = [
     <Icon size={2} tintColor={COLORS._FDCB44} icon={Icons.ICON_BROSWER} />,
     <Icon size={2} tintColor={COLORS._FDCB44} icon={Icons.ICON_FAVORITE} />,
@@ -66,34 +67,3 @@ const CustomTabar: FC<BottomTabBarProps> = ({ state, descriptors, navigation }: 
 };
 
 export { CustomTabar };
-
-export const style = () => {
-  const insets = useSafeAreaInsets();
-
-  return StyleSheet.create({
-    container: {
-      width: Platform.deviceWidth,
-      height: Platform.SizeScale(90),
-      backgroundColor: COLORS._F1F1F1,
-    },
-    content: {
-      flex: 1,
-      flexDirection: 'row',
-    },
-    tabBar: {
-      flex: 1,
-      alignItems: 'center',
-      justifyContent: 'center',
-      zIndex: 999,
-      marginTop: Platform.SizeScale(30),
-    },
-    tabBarHome: {
-      width: Platform.SizeScale(63),
-      height: Platform.SizeScale(63),
-      borderRadius: Platform.SizeScale(63 / 2),
-      backgroundColor: COLORS._FDCB44,
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-  });
-};
