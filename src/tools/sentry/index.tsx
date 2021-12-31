a0be2194b319ac57bc54c4e88add85734dea639e
LongNN hisoft <longnn17.developer@gmail.com>
2021-12-31 23:42:43 +0700

    change code push key

--- src/tools/sentry/index.tsx
+++ src/tools/sentry/index.tsx
@@ -3,25 +3,39 @@ import { SentryContext } from './context';
 import { SentryContextValue, SentryProps } from './types';
 import * as Sentry from '@sentry/react-native';
 import Config from 'react-native-config';
+import { version as packageVersion } from '../../../package.json';
 
 const SentryProvider = ({ children }: SentryProps) => {
-  const routingInstrumentation = useMemo(() => new Sentry.ReactNavigationInstrumentation(), []);
+  const routingInstrumentation = useMemo(
+    () =>
+      new Sentry.ReactNavigationInstrumentation({
+        // How long it will wait for the route change to complete. Default is 1000ms
+        routeChangeTimeoutMs: 500,
+      }),
+    [],
+  );
 
   useEffect(() => {
     Sentry.init({
       dsn: Config.SENTRY_DNS,
-      debug: true,
-      enableAutoSessionTracking: true,
-      tracesSampleRate: 1.0,
+      release: packageVersion,
+      environment: 'dev',
+      beforeSend: e => {
+        return e;
+      },
       integrations: [
         new Sentry.ReactNativeTracing({
-          tracingOrigins: ['localhost', 'api.shadowgate.io', Config.API_COIN, Config.API_URL, /^\//],
+          tracingOrigins: ['localhost', /^\//, /^https:\/\//],
           // Pass instrumentation to be used as `routingInstrumentation`
           routingInstrumentation,
           // To set a uniform sample rate
           // ...
         }),
       ],
+      tracesSampleRate: 1.0,
+      enableAutoSessionTracking: true, // For testing, session close when 5 seconds (instead of the default 30) in the background.
+      sessionTrackingIntervalMillis: 5000,
+      maxBreadcrumbs: 150, // Extend from the default 100 breadcrumbs.
     });
   }, [routingInstrumentation]);
   const contextValue = useMemo<SentryContextValue>(
