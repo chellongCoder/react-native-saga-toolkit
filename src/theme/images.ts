21ae2a4d5e6a28f338f675e17dc54112d9fcf49e
LongNN hisoft <longnn17.developer@gmail.com>
2022-01-04 01:07:37 +0700

    add slider carosel, remove image unused, add header

--- src/theme/images.ts
+++ src/theme/images.ts
@@ -1,4 +1,16 @@
-import TABBAR from './../../assets/images/tabbar.png';
+import IMAGE_LIKE from './../../assets/images/image_like.png'
+import IMAGE_QR from './../../assets/images/image_qr.png'
+import IMG_COMING_SOON from './../../assets/images/img_coming_soon.png'
+import IMG_HEADER from './../../assets/images/img_header.png'
+import LOGO from './../../assets/images/logo.png'
+import TABBAR from './../../assets/images/tabbar.png'
+import TEXT_LOGO from './../../assets/images/text_logo.png'
 export const Images = {
-  TABBAR,
-};
+IMAGE_LIKE,
+IMAGE_QR,
+IMG_COMING_SOON,
+IMG_HEADER,
+LOGO,
+TABBAR,
+TEXT_LOGO,
+}
