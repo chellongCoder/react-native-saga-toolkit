30b82c9a9eada1e7dd6bdb2d293e55b5ffa48833
chuhoanglong <hoanglong@MacBook-Pro-cua-Hoang.local>
2022-01-04 23:49:09 +0700

    fix conflic

--- src/components/product-list-item/index.tsx
+++ src/components/product-list-item/index.tsx
@@ -1,6 +1,10 @@
 import { Text } from '@components/text';
+import { Touchable } from '@components/touchable';
 import { View } from '@components/view';
+import { useNavigation } from '@react-navigation/native';
+import { StackNavigationProp } from '@react-navigation/stack';
 import { ProductItemT } from '@redux/product/types';
+import { ScreenRouteT } from '@routes/types';
 import { COLORS } from '@theme/colors';
 import commonStyles from '@theme/commonStyles';
 import { Images } from '@theme/images';
@@ -15,39 +19,45 @@ type Props = {
 };
 const _ProductListItem = ({ item }: Props) => {
   const styles = useProductListItemStyle();
+  const navigation = useNavigation<StackNavigationProp<ScreenRouteT, 'Home'>>();
+  const onPress = () => {
+    navigation.navigate('ProductDetail');
+  }
   return (
-    <View mr={Platform.SizeScale(10)} style={styles.container}>
-      <View
-        style={{
-          width: Platform.SizeScale(186),
-          height: Platform.SizeScale(131),
-        }}
-      >
-        <FastImage resizeMode={'contain'} style={commonStyles.image} source={Images.IMG_PRODUCT_ITEM} />
-      </View>
-      <View mv={Platform.SizeScale(10)} mh={Platform.SizeScale(10)}>
-        <Text color={COLORS._260001} fontType={'fontBold'} numberOfLines={2} maxWidth={180}>
-          {item.title.toLocaleUpperCase()}
-        </Text>
-        <Text color={COLORS.GRAY} mv={Platform.SizeScale(10)}>
-          {item.sold} đã bán
-        </Text>
-        <View style={[commonStyles.row, commonStyles.spaceBetween]}>
-          <Text color={COLORS._7F2B81}>
-            {item.price} {item.currency}
-          </Text>
-          <Text
-            style={{
-              textDecorationLine: 'line-through',
-            }}
-            fontSize={Platform.SizeScale(12)}
-          >
-            {item.salePrice} {item.currency}
-          </Text>
+    <Touchable onPress={onPress}>
+      <View mr={Platform.SizeScale(10)} style={styles.container}>
+        <View
+          style={{
+            width: Platform.SizeScale(186),
+            height: Platform.SizeScale(131),
+          }}
+        >
+          <FastImage resizeMode={'contain'} style={commonStyles.image} source={Images.IMG_PRODUCT_ITEM} />
         </View>
+        <View mv={Platform.SizeScale(10)} mh={Platform.SizeScale(10)}>
+          <Text color={COLORS._260001} fontType={'fontBold'} numberOfLines={2} maxWidth={180}>
+            {item.title.toLocaleUpperCase()}
+          </Text>
+          <Text color={COLORS.GRAY} mv={Platform.SizeScale(10)}>
+            {item.sold} đã bán
+          </Text>
+          <View style={[commonStyles.row, commonStyles.spaceBetween]}>
+            <Text color={COLORS._7F2B81}>
+              {item.price} {item.currency}
+            </Text>
+            <Text
+              style={{
+                textDecorationLine: 'line-through',
+              }}
+              fontSize={Platform.SizeScale(12)}
+            >
+              {item.salePrice} {item.currency}
+            </Text>
+          </View>
+        </View>
+        <Tag />
       </View>
-      <Tag />
-    </View>
+    </Touchable>
   );
 };
 
